9956442d58a8faeefb5e00170d189ae841fa044c
Lauren Fitzgerald <elarenee@umich.edu>
2015-07-27 18:10:47 -0400

    src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -5,5 +5,25 @@ package com.thoughtworks.tw101.exercises.exercise1;
 public class Main {
     public static void main(String[] args) {
 
+        printAllOddNumsFrom1to100AndSum();
+    }
+
+    private static void printAllOddNumsFrom1to100AndSum() {
+        int x = 0;
+        int sum = 0;
+        while (x < 100) {
+            if (isOdd(x)) {
+                System.out.println(x);
+                sum += x;
+            }
+            x++;
+        }
+        System.out.println("The sum of all odd numbers 1-100 is "+sum+".");
+    }
+
+    public static boolean isOdd(int n) {
+        if(n % 2 != 0)
+            return true;
+        return false;
     }
 }
