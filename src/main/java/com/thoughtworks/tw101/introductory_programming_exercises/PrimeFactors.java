1d4e407a57f158e449e2bdbb41fe853561358781
Lauren Fitzgerald <elarenee@umich.edu>
2015-07-29 21:02:50 -0400

    PrimeFactors completed

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -2,18 +2,44 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 
 //  Prime Factors Exercise
 //  Write a method generate(int n) that given an integer N will return a list of integers such that the numbers
-//  are the factors of N and are arranged in increasing numerical order.
+//  are the prime factors of N and are arranged in increasing numerical order.
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
-import java.util.List;
+import java.util.IntSummaryStatistics;
+import java.util.*;
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        /*List<Integer> primeFactors2 = generate(0);
+        List<Integer> primeFactors3 = generate(256);
+        List<Integer> primeFactors4 = generate(17);
+
+        System.out.println(primeFactors);
+        System.out.println(primeFactors2);
+        System.out.println(primeFactors3);
+        System.out.println(primeFactors4);*/
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> factors = new ArrayList<Integer>();
+        int divisor = 2;
+        while(divisor <= n/2) {
+            if(n % divisor == 0 && isPrime(divisor))
+                factors.add(divisor);
+            divisor++;
+        }
+        return factors;
+    }
+
+    private static boolean isPrime(int n) {
+        int i = 2;
+        while (i <= n/2) {
+            if(n % i == 0)
+                return false;
+            i++;
+        }
+        return true;
     }
 }
