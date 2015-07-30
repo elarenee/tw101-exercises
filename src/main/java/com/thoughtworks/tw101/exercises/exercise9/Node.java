be1fd803457fdc209b1c27551afc0cc34c496cd9
Lauren Fitzgerald <elarenee@umich.edu>
2015-07-29 20:14:25 -0400

    still working on names() method

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,13 +1,11 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
-import java.util.List;
+import java.util.*;
 
 public class Node {
     private String name;
     private Node left;
     private Node right;
-    //maybe not necessary...
-    // private Node parent;
 
     public Node(String name) {
         this.name = name;
@@ -17,7 +15,7 @@ public class Node {
         if(name.compareTo(nameOfNewNode) < 0) {
             if(right == null) {
                 right = new Node(nameOfNewNode);
-                System.out.println("inserted " + nameOfNewNode + " after " + name);
+                //System.out.println("inserted " + nameOfNewNode + " after " + name);
                 return;
             }
             right.add(nameOfNewNode);
@@ -25,7 +23,7 @@ public class Node {
         else {
             if(left == null) {
                 left = new Node(nameOfNewNode);
-                System.out.println("inserted " + nameOfNewNode + " before " + name);
+                //System.out.println("inserted " + nameOfNewNode + " before " + name);
                 return;
             }
             left.add(nameOfNewNode);
@@ -33,6 +31,29 @@ public class Node {
     }
 
     public List<String> names() {
-        return null;
+        //System.out.println(this.name + " " + this.right.name + " " + this.right.right.name+ " " + this.right.right.right.name);
+        //return null;
+        ArrayList nameList = namesHelper(new ArrayList<String>(), this.left);
+        return namesHelper(nameList, this.right);
+    }
+
+    private ArrayList<String> namesHelper(ArrayList<String> nameList, Node node) {
+        if(node.left != null) {
+            return namesHelper(nameList, node.left);
+        }
+        else {
+            nameList.add(node.name);
+            if(node.right != null) {
+                return namesHelper(nameList, node.right);
+            }
+            else {
+                return nameList;
+            }
+        }
+
+
+
+        //System.out.println("Should never get here!");
+        //return null;
     }
 }
